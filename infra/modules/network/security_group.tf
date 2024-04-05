b972601ad10c70da682753e88788273a44b0da6d
sugiyama404 <mmingriha@gmail.com>
2024-04-05 11:15:57 +0900

    up

--- infra/modules/network/security_group.tf
+++
@@ -1,8 +0,0 @@
-# SecurityGroup for RDS
-resource "aws_security_group" "rds_sg" {
-  name   = "rds-sg"
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.app_name}-rds-sg"
-  }
-}
--- infra/modules/rds/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "app_name" {}
-variable "db_sbg_name" {}
-variable "sg_rds_sg_id" {}
-variable "db_ports" {}
-variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
