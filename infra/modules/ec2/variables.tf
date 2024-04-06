d38ae0dafa75584dce1e30f29da3813cf58d8728
sugiyama404 <mmingriha@gmail.com>
2024-04-06 19:57:49 +0900

    gitignore change

--- infra/modules/ec2/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "app_name" {}
-variable "sg_opmng_id" {}
-variable "subnet_public_subnet_1a_id" {}
-variable "sorce_db_address" {}
-variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
