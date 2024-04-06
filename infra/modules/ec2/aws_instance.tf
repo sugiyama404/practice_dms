3fa1c9aaa48392da7f657e2a9fb9cf3a5f623670
sugiyama404 <mmingriha@gmail.com>
2024-04-06 19:58:10 +0900

    up

---
+++ infra/modules/ec2/aws_instance.tf
@@ -0,0 +1,64 @@
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.app_name}-keypair"
+  public_key = file("./modules/ec2/src/keypair.pub")
+
+  tags = {
+    Name = "${var.app_name}-keypair"
+  }
+}
+
+resource "aws_instance" "opmng_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = var.subnet_public_subnet_1a_id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [
+    var.sg_opmng_id
+  ]
+  key_name  = aws_key_pair.keypair.key_name
+  user_data = <<EOF
+#!/bin/bash
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+sudo yum update -y
+sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el8-9.noarch.rpm
+sudo yum install -y yum-utils
+sudo yum-config-manager --disable mysql80-community
+sudo yum-config-manager --enable mysql57-community
+sudo yum install -y mysql-community-client
+sudo yum install mysql -y
+
+sleep 700
+
+mysql -h${var.sorce_db_address} \
+  -D${var.db_name} \
+  -u${var.db_username} \
+  -p${var.db_password} \
+  -e "
+CREATE TABLE IF NOT EXISTS users (
+id INT AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(255) DEFAULT NULL,
+email VARCHAR(255) DEFAULT NULL,
+password VARCHAR(255) DEFAULT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);"
+
+mysql -h${var.sorce_db_address} \
+  -D${var.db_name} \
+  -u${var.db_username} \
+  -p${var.db_password} \
+  -e "
+INSERT INTO users (username, email, password) VALUES
+('alice', 'alice@example.com', 'password123'),
+('bob', 'bob@example.com', 'password456'),
+('charlie', 'charlie@example.com', 'password789'),
+('david', 'david@example.com', 'password012'),
+('eve', 'eve@example.com', 'password345');
+"
+EOF
+
+  tags = {
+    Name = "${var.app_name}-opmng-ec2"
+    Type = "app"
+  }
+}
---
+++ infra/modules/ec2/variables.tf
@@ -0,0 +1,7 @@
+variable "app_name" {}
+variable "sg_opmng_id" {}
+variable "subnet_public_subnet_1a_id" {}
+variable "sorce_db_address" {}
+variable "db_name" {}
+variable "db_username" {}
+variable "db_password" {}
